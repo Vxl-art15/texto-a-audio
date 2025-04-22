0b368647b79ac1583395539ad35b231abf2036f5
Vxl-art15 <valeria15260323@gmail.com>
2025-04-21 20:25:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+st.title("Generación de Audio desde Texto")
+image = Image.open('Irelia.jpg')
 st.image(image, width=350)
 with st.sidebar:
-    st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
+    st.subheader("Escribe o selecciona texto para convertirlo en audio.")
 
 
 try:
@@ -19,18 +19,15 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
+st.subheader("Una pequeña frase.")
+st.write('"Nuestra tierra es una de magia. Una por la que vale la pena morir." ' 
          '  '
-         ' Franz Kafka.'
+         ' Irelia.'
         
         )
            
-st.markdown(f"Quieres escucharlo?, copia el texto")
-text = st.text_area("Ingrese El texto a escuchar.")
+st.markdown(f"¿Quieres escucharlo? Copia el texto aquí:")
+text = st.text_area("Escriba aquí el texto que desea escuchar")
 
 tld='com'
 option_lang = st.selectbox(
